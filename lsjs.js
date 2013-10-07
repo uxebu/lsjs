ebdc6d34ee1bbc6d5574e0b74135d32050d36c64
Wolfram Kriesing <wk@uxebu.com>
2013-10-07 17:37:15 +0200

    Externalized LocalStorage implementation.

--- lsjs.js
+++ lsjs.js
@@ -29,81 +29,12 @@ var define;
 		}
 	};
 
-	lsImpl = {
-		isSupported: function() {
-			try {
-				return 'localStorage' in window && window['localStorage'] !== null;
-			} catch (e) {
-				return false;
-			}
-		},
-		remove: function(key, handler, errorHandler) {
-			try {
-				var json = localStorage[key];
-				if (json !== undefined && json !== null) {
-					var value = JSON.parse(json);
-					localStorage.removeItem(key);
-					if (handler) {
-						handler(value);
-					}
-				} else {
-					if (errorHandler) {
-						errorHandler("Failed to remove value in local storage ["+key+"]");
-					} else {
-						console.log("Failed to remove value in local storage ["+key+"]");
-					}
-				}
-			} catch(e) {
-				if (errorHandler) {
-					errorHandler(e);
-				} else {
-					console.log("Failed to remove value in local storage ["+key+"] : "+e);
-				}
-			}
-		},
-		get: function(key, handler, errorHandler) {
-			try {
-				var json = localStorage[key];
-				if (json !== undefined && json !== null) {
-					var value = JSON.parse(json);
-					handler(value);
-				} else {
-					if (errorHandler) {
-						errorHandler("Failed to get value in local storage ["+key+"]");
-					} else {
-						console.log("Failed to get value in local storage ["+key+"]");
-					}
-				}
-			} catch(e) {
-				if (errorHandler) {
-					errorHandler(e);
-				} else {
-					console.log("Failed to get value in local storage ["+key+"] : "+e);
-				}
-			}
-		},
-		set: function(key, entry, handler, errorHandler) {
-			try {
-				localStorage[key] = JSON.stringify(entry);
-				if (handler) {
-					handler(true);
-				}
-			} catch (e) {
-				if (errorHandler) {
-					errorHandler(e);
-				} else {
-					console.log("Failed to set value in local storage ["+key+"] : "+e);
-				}
-			}
-		}
-	};
-
 	var modules = {};
 	var moduleStack = [];
 	var paths = {};
 	var pkgs = {};
 	var reload = {};
-	var storage = lsImpl;
+	var storage;
 	var loaded = {};
 	var cache = {};
 	var cachets = {};
