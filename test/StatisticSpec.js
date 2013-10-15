82ab8f50396702c8d551368eb9216da0a8dc528e
Georg Lang <georglang0788@gmx.net>
2013-10-15 10:20:16 +0200

    Implemented getDependencies and storeDependency TDD style. Added jasmine-matchers to package json.

--- test/StatisticSpec.js
+++ test/StatisticSpec.js
@@ -1,15 +1,19 @@
-function Statistic (){
 
+function Statistic (){
+  this.dependencies = [];
 }
 
 Statistic.prototype = {
-  storeDependency: function(){
-
+  storeDependency: function(fileName){
+    if(this.dependencies.indexOf(fileName) == -1) {
+      this.dependencies.push(fileName);
+    }
   },
+
   getDependencies : function(){
-    return ['main.js'];
+    return this.dependencies;
   }
-}
+};
 
 describe('Statistic', function(){
   describe('storeDependency()', function(){
@@ -18,8 +22,36 @@ describe('Statistic', function(){
       stats.storeDependency('main.js');
       expect(stats.getDependencies())
           .toEqual(['main.js']);
-    })
+    });
+
+    it('should store two dependencies',function(){
+      var stats = new Statistic;
+      stats.storeDependency('main.js');
+      stats.storeDependency('test1.js');
+      expect(stats.getDependencies())
+          .toEqual(['main.js', 'test1.js']);
+    });
+    it('should store a dependency once',function(){
+      var stats = new Statistic;
+      stats.storeDependency('main.js');
+      stats.storeDependency('main.js');
+      expect(stats.getDependencies())
+          .toEqual(['main.js']);
+    });
+    it('should store every dependency once',function(){
+      var stats = new Statistic;
+      stats.storeDependency('test.js');
+      stats.storeDependency('main.js');
+      stats.storeDependency('test.js');
+      stats.storeDependency('main.js');
+      stats.storeDependency('test2.js');
+      stats.storeDependency('test2.js');
+      expect(stats.getDependencies())
+          .toContainEachOnce(['main.js', 'test.js', 'test2.js']);
+    });
   });
+
+
 
   describe('getDependencies()', function(){
 
